2c4a430f9bc0576ea69e969ddec796f4da560a64
ecelias <elizabeth.elias@uiowa.edu>
2024-09-01 17:53:09 +0000

    vFinalFinalActuallyFinalVersion 9/1/2024

--- A1/run.py
+++ A1/run.py
@@ -26,21 +26,17 @@ if len(sys.argv) == 2:
     for command in commands:
         try:
             current_command = sp.run(command, capture_output=True, text=True)
-            to_output_file(f'Command: <{command}> {current_command.stdout}')
+            to_output_file(f'Command: {command} {current_command.stdout}')
         except sp.CalledProcessError as e:
-            to_output_file(f'[Error] Command: <{command}> failed with return code: {e.returncode}')
-            print(f'[Error] Command <{command}> failed with return code: {e.returncode}')
+            to_output_file(f'[Error] Command: {command} failed with return code: {e.returncode}')
+            print(f'[Error] Command {command} failed with return code: {e.returncode}')
 
 elif len(sys.argv) == 1:
-    to_output_file('[Error] No IP address found in system input. Please try again.')
-    print('[Error] No IP address found in system input. Please try again.')
+    to_output_file('[Error] No IP address define in system input. Please try again.')
+    print('[Error] No IP address defined in system input. Please try again.')
     
 elif len(sys.argv) > 2:
     to_output_file('[Error] Too many arguments detected. Please try again.')
     print('[Error]Too many arguments detected. Please try again.')
-
-elif len(sys.argv) < 1:
-    to_output_file('[Error] Script name and IP address not defined. Please try again.')
-    print('[Error] Script name and IP address not defined. Please try again.')
 
 file1.close()
