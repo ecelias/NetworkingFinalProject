af6c6a30614527403dd65c3d2e04e85606de6ad6
ecelias <elizabeth.elias@uiowa.edu>
2024-09-09 23:54:56 +0000

    Updated Task 1 - 240909

--- A2/server.py
+++ A2/server.py
@@ -1,32 +1,41 @@
 import sys 
 import socket
+from _thread import *
+import threading
 
-if len(sys.argv) == 2:
-    port = sys.argv[1]
-    
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    default_port = int(port)
-    
-    try:
-        host_ip = socket.gethostbyname("127.0.0.1")
-        print(host_ip)
-    except socket.gaierror:
-        print("Error resolving host")
-        sys.exit()
+## used https://www.geeksforgeeks.org/socket-programming-multi-threading-python/ to learn syntax for multi-threading
+## used https://www.geeksforgeeks.org/socket-programming-python/ to learn more about the syntax for socket programming 
 
-    tcp.bind((host_ip, default_port))
-    print(f'The socket has successfully connected to {default_port}')
+
+def main():
+    if len(sys.argv) == 2:
+        port = sys.argv[1]
     
-    tcp.listen(0)
-    print(f"Listening on {host_ip}:{default_port}")
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    while True:
-        cli, address = tcp.accept()
-        print(f'Connection from {address} received.')
+        default_port = int(port)
+    
+        try:
+            host_ip = socket.gethostbyname("127.0.0.1")
+            print(host_ip)
+        except socket.gaierror:
+            print("Error resolving host")
+            sys.exit()
+
+        tcp.bind((host_ip, default_port))
+        print(f'The socket has successfully connected to {default_port}')
+    
+        tcp.listen(6)
+        print(f"Listening on {host_ip}:{default_port}")
+    
+        while True:
+            connection, address = tcp.accept()
+            print(f'Connection from {address} received.')
         
-        cli.send("You've connected to the server".encode())
+            connection.send("You've connected to the server".encode())
         
-        cli.close()
-        break
-    
+            connection.close()
+            break
+        
+if __name__ == "__main__":
+    main()
