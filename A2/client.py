420db7b45bca2732b9a2ae9476f6fd134f0f0088
ecelias <elizabeth.elias@uiowa.edu>
2024-09-10 02:21:17 +0000

    Task 2 v1, fix message format from sender 240909

--- A2/client.py
+++ A2/client.py
@@ -1,21 +1,45 @@
 import sys 
 import socket
+import threading
+from _thread import *
 
+HEADER = 64
+FORMAT = 'ascii'
+DISCONNECT_MESSAGE = '/quit'
+
+def recv_thread(cli_socket):
+    while True:
+        try: 
+            data = cli_socket.recv(1024)
+            if data == DISCONNECT_MESSAGE:
+                break
+            print(f"Received from server: {data.decode(FORMAT)}")
+        except:
+            break
+        
+def send_thread(message, cli_socket):
+    cli_socket.send(message.encode(FORMAT))
+    
 def main():
     if len(sys.argv) == 3:
         port = int(sys.argv[2])
         host_name = str(sys.argv[1])
-        print(host_name)
     
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-        server_ip = "127.0.0.1"
-        server_port = 5000
-    
         client.connect((host_name, port))
-    
-        print(client.recv(5000).decode())
-    
+        
+        start_new_thread(recv_thread, (client,))
+        
+        while True:
+            msg = input("Input message: ")
+            start_new_thread(send_thread, (msg, client,))
+            
+            if msg == DISCONNECT_MESSAGE:
+                break
+            else: 
+                continue
+            
         client.close()
 
 if __name__ == "__main__":
--- A2/server.py
+++ A2/server.py
@@ -6,6 +6,37 @@ import threading
 ## used https://www.geeksforgeeks.org/socket-programming-multi-threading-python/ to learn syntax for multi-threading
 ## used https://www.geeksforgeeks.org/socket-programming-python/ to learn more about the syntax for socket programming 
 
+file1 = open('/home/ecelias/cs3640/A2/output.txt', 'w+')
+file1.write('\n')
+file1.write('\n')
+
+def write_to_file(message):
+    file1.write(message)
+    file1.write('\n')
+
+HEADER = 64
+FORMAT = 'ascii'
+DISCONNECT_MESSAGE = '/quit'
+
+clients = set()
+    
+def threaded(connection):
+    clients.add(connection)
+    while True:
+        data = connection.recv(1024)
+        recv_msg = f'Message received from {connection}: {data}'
+        write_to_file(recv_msg)
+        if not data:
+            print("Goodbye")
+            quit_msg = f'{connection} has disconnected.'
+            write_to_file(quit_msg)
+            break
+        else:
+            for client in clients:
+                sent_msg = f'Message from {connection} sent: {data}'
+                write_to_file(sent_msg)
+                client.send(data)
+    connection.close()
 
 def main():
     if len(sys.argv) == 2:
@@ -17,7 +48,6 @@ def main():
     
         try:
             host_ip = socket.gethostbyname("127.0.0.1")
-            print(host_ip)
         except socket.gaierror:
             print("Error resolving host")
             sys.exit()
@@ -30,12 +60,14 @@ def main():
     
         while True:
             connection, address = tcp.accept()
-            print(f'Connection from {address} received.')
-        
-            connection.send("You've connected to the server".encode())
-        
-            connection.close()
-            break
+            conn_accepted_msg = f'Connection from {address} received.'
+            file1.write(conn_accepted_msg)
+            print(conn_accepted_msg)
+
+            start_new_thread(threaded, (connection,))
+            #print(f'Current active connections: {threading.active_count()}')
+            
+    tcp.close()
         
 if __name__ == "__main__":
     main()
